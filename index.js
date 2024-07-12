b5db3a0a14d7961dece45893d17022aee166e307
mymircle0118 <noahsflood908@gmail.com>
2024-07-13 05:39:34 +0900

    finish update stellar toml file and add nginx config

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const ws = require("ws");
 const fs = require("fs");
+const os = require('os'); 
 
 require("dotenv").config();
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const http = require("http");
 const server = http.createServer(app);
 
 const stellar_toml_path = process.env.STELLAR_TOML_FILE_PATH;
 
 function insertNewNFTtoTOML(code, issuer, name, desc, image, display_decimals) {
+
+  const dirPath = `${stellar_toml_path}/.well-known`;
+
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+  }
+
+  const fileName = 'stellar.toml';
+  const filePath = `${dirPath}/${fileName}`;
+
+  console.log("filePath", filePath);
+
   let data = "\n[[CURRENCIES]]\n";
   data = data + "code=\"" + code + "\"\n";
   data = data + "issuer=\"" + issuer + "\"\n";
@@ -20,7 +36,7 @@ function insertNewNFTtoTOML(code, issuer, name, desc, image, display_decimals) {
   data = data + "display_decimals=\"" + display_decimals + "\"\n";
   
   try {
-    fs.writeFileSync(stellar_toml_path, data, {flag: 'a+'});
+    fs.writeFileSync(filePath, data, {flag: 'a+'});
     return {
       ok: true,
       error: null
@@ -44,14 +60,16 @@ app.get("/cors", (req, res) => {
 });
 
 app.post("/insert_nft", (req, res) => {
-  const data = req.query;
-  console.log("request", req.query);
+  // console.log('===============');
+  const data = req.body;
+  // console.log("request", req);
+  console.log("====data====", data);
 
   const result = insertNewNFTtoTOML(data.code, data.issuer, data.name, data.desc, data.image, data.decimals);
 
   res.send(result);
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+server.listen(6000, () => {
+  console.log("listening on *:6000");
 });
